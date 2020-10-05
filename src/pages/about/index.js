ba3c6e557af3335be256ce35652d5c55091f4fc3
Azrie Bakri <mohamad.azriebakri@gmail.com>
2020-10-06 02:09:56 +0800

    Cleaned up about section - me

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,8 +1,35 @@
-import { React } from 'react';
+import React from 'react';
+import "./index.scss"
 
+import MeJPG from "../../assets/images/me.jpg"
 
 function About(){
-    return("this is the about page");
+    return(
+        <div className="page-container">
+            <section className="about-section">
+                <div className="about-section-image animated animatedFadeInUp fadeInUp">
+                    <img src={MeJPG}/>
+                </div>
+                <div className="about-section-text animated animatedFadeInUp fadeInUp">
+                    <h1 className="about-header">
+                        I’m Azrie, a fullstack developer who is 
+                        currently working for Service Rocket
+                        as a software engineer in Kuala Lumpur, Malaysia.
+                    </h1>
+                    <div className="about-subheader">
+                        I’ve spent the past 3 years+ working accross different areas of system development; 
+                        front-end development for most part, mainly using Reactjs, back-end development with 
+                        Nodejs/Express, databases as well with CI/CD. 
+                    </div>
+                    <div className="about-subheader">
+                        Out of work hours, you’ll find me dreaming of bouldering and rock climbing, 
+                        coding for fun, take photos here and there and picking up skills that would 
+                        make me a better developer.
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default About;
